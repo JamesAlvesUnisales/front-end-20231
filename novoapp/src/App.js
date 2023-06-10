fae4cd2cca671cbc7fe917d819aa626f5bad8f1e
James Alves <james@jamesalves.com.br>
2023-06-10 12:36:41 -0300

    app react

--- novoapp/src/App.js
+++ novoapp/src/App.js
@@ -1,85 +1,93 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Linha from "./componentes/Linha";
-import { useState } from "react";
-
-const res = await fetch("https://jsonplaceholder.typicode.com/users");
-const lista = await res.json();
+import { useState, useEffect } from "react";
 
 function App() {
-  const [pessoa, setPessoa] = useState({
-    name: "James Alves",
-    username: "james.alves",
-  });
+  const [aluno, setAluno] = useState({ id : 0, name: "", RA: "" });
+  const [lista, setLista] = useState([]);
+  const [contador, setContador] = useState(0);
+  const [alerta, setAlerta] = useState("");
+  const [mode, setMode] = useState("create");
+  useEffect(() => {
+    const listaAux = JSON.parse(localStorage.getItem("lista")) ?? [];
+    if (listaAux.length > 0) {
+      setLista(listaAux);
+    }
+  }, []);
 
-  const [postBlog, setPostBlog] = useState({
-    title: '',
-    body: '',
-    userId: '',
-  });
+  useEffect(() => {
+    const aux = lista.find((e) => e.name == aluno.name);
+    if (aux) {
+      setAlerta(`aluno ${aluno.name} ja existe!`);
+    } else {
+      setAlerta("");
+    }
+    //setContador(contador+1);
+  }, [lista, aluno]);
 
-  const handleButton = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: JSON.stringify(postBlog),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    });
-    const payload = await response.json()
-    console.log(payload)
-    alert(`Post ${payload.id} foi criado com sucesso!`)
-  };
-  const changeInputUsername = ({ target }) => {
-    setPessoa({ ...pessoa, username: target.value });
+  const handleSave = () => {
+    console.log(mode)
+    if(mode === 'edit'){
+      let aux = lista.find((item)=> item.id === aluno.id);
+      aux.name  = aluno.name;
+      aux.RA  = aluno.RA;
+      localStorage.setItem("lista", JSON.stringify(lista));
+      setLista(lista)
+    }else{
+      const idx = lista.length + 1;
+      aluno.id = idx;
+      lista.push(aluno);
+      localStorage.setItem("lista", JSON.stringify(lista));
+      setContador(contador + 1);
+      setAluno({ name: "", RA: "" });
+    }
+
   };
 
-  const changeInputName = ({ target }) => {
-    setPessoa({ ...pessoa, name: target.value });
+  const onChangeName = ({ target }) => {
+    setAluno({ ...aluno, name: target.value });
+
   };
 
-  const changeInputUserId = ({ target }) => {
-    setPostBlog({ ...postBlog, userId: target.value });
+  const onChangeRA = ({ target }) => {
+    setAluno({ ...aluno, RA: target.value });
   };
-  const changeInputTitle = ({ target }) => {
-    setPostBlog({ ...postBlog, title: target.value });
-  };
-  const changeInputBody = ({ target }) => {
-    setPostBlog({ ...postBlog, body: target.value });
-  };
+
+  const handleEdit = (aluno)=>{
+    console.log(aluno)
+    setMode('edit')
+    setAluno({...aluno})
+  }
+
   return (
     <>
+      <h1>{aluno.name}</h1>
+      <h2>{contador}</h2>
+      <h2>{alerta}</h2>
+      <h3>{aluno.id}</h3>
+      <input
+        type="text"
+        placeholder="name"
+        value={aluno.name}
+        onChange={onChangeName}
+      ></input>
+      <input
+        type="text"
+        placeholder="RA"
+        value={aluno.RA}
+        onChange={onChangeRA}
+      ></input>
+      <button onClick={handleSave}>Salvar</button>
+
       <div>
-        <input placeholder="user id" value={postBlog.userId} onChange={changeInputUserId}></input>
-        <input  placeholder="title"  value={postBlog.title} onChange={changeInputTitle}></input>
-        <textarea  placeholder="body" value={postBlog.body} onChange={changeInputBody}></textarea>
-        <button onClick={handleButton}>Salvar</button>
-      </div>
-      <div>
-        <div>Ola {pessoa.name}!</div>
-
-        <input
-          type="text"
-          id="username"
-          placeholder="usuario"
-          value={pessoa.username}
-          onChange={changeInputUsername}
-        />
-
-        <input
-          type="text"
-          placeholder="nome"
-          value={pessoa.name}
-          onChange={changeInputName}
-        />
-
-        <button onClick={() => handleButton()}>Salvar</button>
-        <div>Adeus {pessoa.username}!</div>
-      </div>
-      <div className="grade">
-        {lista.map((userInfo) => {
+        {lista.map((item, index) => {
           return (
-            <Linha nome={userInfo.name} id={userInfo.id} objeto={userInfo} />
+            <div>
+              <div key={index}>
+                {item.name} - {item.RA} - <button onClick={(event)=>{ handleEdit(item)}}>Editar</button> <button>Excluir</button>
+              </div>
+            </div>
           );
         })}
       </div>
